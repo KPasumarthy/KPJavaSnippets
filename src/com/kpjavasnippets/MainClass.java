e19d7d73ecfb7a917a81aad87383fd67981ada8e
Kailash Pasumarthy <KPasumarthy@gmail.com>
2020-06-18 10:40:49 -0400

    KP : KPJavaSnippets Main Class with main() method added!

---
+++ src/com/kpjavasnippets/MainClass.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.kpjavasnippets;
+
+/**
+ * @author admin
+ *
+ */
+public class MainClass {
+
+	/**
+	 * KP : MainClass Constructor!
+	 */
+	public MainClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("KP : KPJavaSnippets : MainClass : main()");
+	}
+
+}
